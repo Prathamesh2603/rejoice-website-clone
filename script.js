b3fb53c43e1a6ca1384e3fdcd0ec99dd37a2013b
Prathamesh2603 <prathameshpawar645@gmail.com>
2026-02-12 20:46:17 +0530

    swiper js code for default infinite code is added from swiperjs site to the index, style and script

--- script.js
+++ script.js
@@ -95,3 +95,17 @@ const page4Animation = () => {
 };
 
 page4Animation();
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    },
+});
